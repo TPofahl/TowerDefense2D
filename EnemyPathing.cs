a04985b4dca27c4c495094256d2de300d4a4c839
TPofahl <thomaspofahl@yahoo.com>
2022-12-18 13:20:18 -0600

    Created enemy spawner that deletes off map

---
+++ EnemyPathing.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class EnemyPathing : Node2D {
+	[Export]
+	public int EnemySpeed = 100;
+	public PathFollow2D EnemyPath;
+
+	public override void _Ready()
+	{
+		EnemyPath = GetNode<PathFollow2D>("EnemyLine/EnemyPath");
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+  	public override void _Process(float delta) {
+		
+	EnemyPath.SetOffset(EnemyPath.GetOffset() + EnemySpeed * delta);
+	if (EnemyPath.GetUnitOffset() == 1) {
+		QueueFree();
+		GD.Print("enemy deleted");
+	}
+	
+  }
+}
--- Level1.cs
+++ Level1.cs
@@ -1,21 +1,23 @@
 using Godot;
 using System;
 
-public class Map1 : Node2D
+public class Level1 : Node2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		// var Bush = (Bush)ResourceLoader.Load("res://Bush.tscn");
+	[Export]
+	float spawnTime = 2;
+	float timer = 0;
+	
+	//PackedScene enemy = ResourceLoader.Load<PackedScene>("res://Scenes/Enemy.tscn").Instance();
+	PackedScene enemy = GD.Load<PackedScene>("res://Scenes/EnemyPathing.tscn");
+		
+  	// Called every frame. 'delta' is the elapsed time since the previous frame.
+  	public override void _Process(float delta) {
+	timer = timer + delta;
+	if (timer > spawnTime) {
+		var newEnemy = enemy.Instance();
+		AddChild(newEnemy);
+		timer = 0;
+		GD.Print("timer reset");
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	}
 }
