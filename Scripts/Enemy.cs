85def0dbfcb62e0cdc80720da418c6edd8936a7f
TPofahl <thomaspofahl@yahoo.com>
2023-01-06 19:04:24 -0600

    Fixed healthbar rotating with enemy

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -5,27 +5,35 @@ public class Enemy : Area2D
 {
     [Signal]
     delegate void EnemyDestroyed();
-    int health = 100;
-    TextureProgress healthBar;
+    [Export]
+    public int health = 100;
+    [Export]
+    public Vector2 healthBarOffset = new Vector2(-25,35);
+    TextureProgress healthTexture;
+    Node2D healthBar;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        healthBar = GetNode<TextureProgress>("HealthBar/TextureProgress");
-        healthBar.Value = health;
+        healthTexture = GetNode<TextureProgress>("HealthBar/TextureProgress");
+        healthBar = GetNode<Node2D>("HealthBar");
+        healthTexture.Value = health;
+
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  public override void _Process(float delta)
+  {
+    healthBar.GlobalPosition = GlobalPosition + healthBarOffset;
+    healthBar.GlobalRotation = 0;
+
+  }
 
     private void OnEnemyAreaEntered(Node body)
 	{
         if (body.IsInGroup("BulletGroup"))
         {
             health -= 10;
-            healthBar.Value = health;
+            healthTexture.Value = health;
             if (health <= 0) EmitSignal(nameof(EnemyDestroyed));
         }
 	}
